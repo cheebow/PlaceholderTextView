670c733add0124b1bea1f91f5dca84f61e2cace7
CHEEBOW <cheebow@gmail.com>
2015-07-24 13:47:16 +0900

    Add sources

---
+++ PlaceholderTextView/PlaceholderTextView/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PlaceholderTextView
+//
+//  Created by CHEEBOW on 2015/07/24.
+//  Copyright (c) 2015年 CHEEBOW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let placeholderTextView = PlaceholderTextView()
+        let frame = CGRectMake(0.0, 20.0, 320.0, 100.0)
+        placeholderTextView.frame = frame
+        placeholderTextView.placeholder = "Placeholder Text"
+        placeholderTextView.placeholderColor = UIColor.lightGrayColor()
+        self.view.addSubview(placeholderTextView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
